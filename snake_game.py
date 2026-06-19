a4215292c05a1af1430fa3c00b0cc0f398a8dd7e
patrickbaidu <baydo.patrickjohndelfin@gmail.com>
2026-06-19 22:36:52 +0800

    In this commit, I made the conditions and the constant system for the pygame to start the window screen.

---
+++ snake_game.py
@@ -0,0 +1,17 @@
+import pygame, sys
+
+pygame.init()
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    screen.fill((175, 215, 70))
+    pygame.display.update()
+    clock.tick(60)
